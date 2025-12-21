5ccb2619e4a28c9cb2bb6fb6d94f337f23755e2a
Ikterab <iyt.uroj777@gmail.com>
2025-12-21 16:37:09 +0600

    4:36 21/12/2025

--- src/Component/Footer/Footer.tsx
+++ src/Component/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const Footer:React.FC = () => {
@@ -37,13 +37,13 @@ const Footer:React.FC = () => {
     const thirdcol = ["Partner", "Support"]
 
     return (
-        <footer className=" w-full  max-w-screen-3xl bg-[black] text-[white]  ">
-            <div className="flex w-full max-w-screen-3xl mx-auto xl:flex-nowrap flex-wrap justify-between py-10">
+        <footer className=" w-full   bg-[black] text-[#d9d8d8]  ">
+            <div className="flex w-full  mx-auto xl:flex-nowrap flex-wrap justify-between py-10">
                     <div className="flex flex-col px-10 py-10 items-start">
-                        <p>Demian</p>
-                        <p>Hassle-free blogging platform that developers and teams love.</p>
+                        <p className="text-[40px] font-semibold">Demian</p>
+                    <p className="text-[#bbb8b8]">Hassle-free blogging platform that developers and teams love.</p>
                     </div>
-                <div  className="flex  justify-center  2xl:gap-40 gap-10 2xl:px-45 px-10 lg:pt-10 pt-10">
+                <div  className="flex flex-col sm:flex-row  justify-center  2xl:gap-40 gap-10 2xl:px-45 px-10  lg:pt-10 pt-10">
                         {/* <div className="flex lg:flex-nowrap flex-wrap lg:gap-10 gap-3 ">
                             {Object.entries(footerItems).map(([title,items]) => (
                                 <div className="flex flex-col items-start" key={title}>
@@ -68,7 +68,7 @@ const Footer:React.FC = () => {
                                         {items?.map((item, index) => (
 
                                             <li key={index}
-                                            className="whitespace-nowrap"
+                                                className="whitespace-nowrap text-[#bbb8b8]"
                                             >{item}</li>
 
                                         ))}
@@ -85,7 +85,7 @@ const Footer:React.FC = () => {
                                         <ul className="flex flex-col items-start">
                                             {items?.map((item, index) => (
 
-                                                <li key={index} className="whitespace-nowrap" >{item}</li>
+                                                <li key={index} className="whitespace-nowrap text-[#bbb8b8]" >{item}</li>
 
                                             ))}
                                         </ul>
@@ -101,7 +101,7 @@ const Footer:React.FC = () => {
                                         <ul className="flex flex-col items-start">
                                             {items?.map((item, index) => (
 
-                                                <li key={index} className="whitespace-nowrap" >{item}</li>
+                                                <li key={index} className="whitespace-nowrap text-[#bbb8b8]" >{item}</li>
 
                                             ))}
                                         </ul>
@@ -109,7 +109,24 @@ const Footer:React.FC = () => {
                                 ))}
                         </div>
                     </div>
-              </div>
+            </div>
+            
+            <div className="flex flex-wrap bg-[rgba(63,59,59,0.46)] py-10 sm:justify-between justify-center px-10">
+                <p className="text-[#bbb8b8]"> @ 2024 Demian-LinearBytes Inc.</p>
+                <div className="flex flex-wrap justify-center gap-4 "> 
+                    {
+                        ["Privacy Policy", "Terms", "Codes of conduct"].map((items, index) => (
+                            <Link
+                                key={index}
+                                to={`/${items.toLowerCase()}`}
+                                className="text-[#bbb8b8]"
+                            >
+                                <span className="whitespace-nowrap  hover:text-[#8080b7]">{items}</span>
+                            </Link>))
+                    }
+                </div>
+                    
+            </div>
        </footer>
    ) 
 }
